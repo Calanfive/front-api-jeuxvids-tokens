03b527d5d5f36345dc1399e106add0cff806d31b
calanfive <camille.lelan@gmail.com>
2023-12-09 16:45:20 +0100

    new page modifMDP

---
+++ src/routes/Modifmdp.tsx
@@ -0,0 +1,21 @@
+
+export default function modifMdp() {
+
+    
+    return (
+        <div>
+            <div className="form-field">
+                <label htmlFor="password">Entrez votre mot de passe actuel :</label>
+                {/* <input type="text" name="login" value={login} onChange={changeLogin} /> */}
+            </div>
+            <div className="form-field">
+                <label htmlFor="password">Entrez votre nouveau mot de passe : </label>
+                {/* <input type="password" name="password" value={password} onChange={changePassword} /> */}
+            </div>
+            <div className="form-field">
+                <label htmlFor="password">Confirmez votre nouveau mot de passe : </label>
+                {/* <input type="password" name="password" value={password} onChange={changePassword} /> */}
+            </div>
+        </div>
+    )
+};
